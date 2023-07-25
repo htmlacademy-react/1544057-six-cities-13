d6dc5a074e2decc83944cfe1e9b1b2bf70733a8c
Original-pokemon <eremkin-02@mail.ru>
2023-07-25 17:57:05 +0300

    feat: BrowserRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,25 @@
-import MainPage from '../../pages/main-page/main-page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-type AppMainProps = {
+import MainPage from '../../pages/main-page/main-page';
+import { AppRoute } from './const';
+
+type AppProps = {
   offersCount: number;
 }
 
-function App({ offersCount }: AppMainProps): JSX.Element {
+function App({ offersCount }: AppProps): JSX.Element {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} >
+          <Route
+            index
+            element={<MainPage offersCount={offersCount} />}
+          />
     <MainPage offersCount={offersCount} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/app/const.tsx
@@ -0,0 +1,13 @@
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer',
+  PageNotFound = '*',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unkhown = 'UNKNOWN'
+}
