433cf7429616c52810b54d50288059eb972ba449
Original-pokemon <eremkin-02@mail.ru>
2023-07-26 21:39:15 +0300

    feat: private-route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import OfferPage from '../../pages/offer-page/offer-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number;
@@ -26,7 +27,13 @@ function App({ offersCount }: AppProps): JSX.Element {
           />
           <Route
             path={AppRoute.Favorites}
-            element={<FavoritesPage />}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <FavoritesPage />
+              </PrivateRoute>
+            }
           />
           <Route
             path={AppRoute.Offer}
