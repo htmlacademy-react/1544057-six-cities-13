bd57156ce4e40067d74367a4d09a9852d8b4bdc3
Original-pokemon <eremkin-02@mail.ru>
2023-07-26 21:51:03 +0300

    feat: add offersCount in main component

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -33,7 +33,7 @@ export default function MainPage({ offersCount }: MainPageProps): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in {CityName.Paris}</b>
+              <b className="places__found">{offersCount} places to stay in {CityName.Paris}</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
