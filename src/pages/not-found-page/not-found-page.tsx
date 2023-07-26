9cf86a90c53ca24baf58876c1a4c933b6c6a7c59
Original-pokemon <eremkin-02@mail.ru>
2023-07-26 18:44:33 +0300

    feat: logo component

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,8 +1,6 @@
 import './not-found-page.css';
 
-import { Link } from 'react-router-dom';
-
-import { AppRoute } from '../../const';
+import Logo from '../../components/logo/logo';
 
 export default function NotFoundPage(): JSX.Element {
   return (
@@ -10,11 +8,7 @@ export default function NotFoundPage(): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to={AppRoute.Main}>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
+            <Logo />
           </div>
         </div>
       </header>
