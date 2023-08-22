cce011a7162348974da4515803f1631b3262c08a
Original-pokemon <eremkin-02@mail.ru>
2023-08-22 18:01:05 +0300

    feat: formatDate

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
-export default function capitalizeFirstLetter(str: string): string {
+function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long' });
+}
+
+export { capitalizeFirstLetter, formatDate };
