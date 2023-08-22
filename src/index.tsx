deb3bf7f67e26710423508277f1b3d0c3332719d
Original-pokemon <eremkin-02@mail.ru>
2023-08-22 11:55:21 +0300

    feat: add mocks

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app';
+import { offers } from './mocks/offers';
+import reviewsMap from './mocks/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const OFFERS_COUNT = 5;
-
 root.render(
   <React.StrictMode>
-    <App offersCount={OFFERS_COUNT} />
+    <App offers={offers} reviewsMap={reviewsMap} />
   </React.StrictMode>
 );
