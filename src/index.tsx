2d723b6f9de36bd45ad6ab965864d559f0577da0
Original-pokemon <eremkin-02@mail.ru>
2023-08-29 16:35:19 +0300

    feat: init store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './components/app/app';
 import { extendedOfferMap, offers } from './mocks/offers';
 import reviewsMap from './mocks/reviews';
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App offers={offers} extendedOfferMap={extendedOfferMap} reviewsMap={reviewsMap} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App
+        offers={offers}
+        reviewsMap={reviewsMap}
+        extendedOfferMap={extendedOfferMap}
+      />
+    </React.StrictMode>
+  </Provider>
 );
