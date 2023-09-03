89ae4df1a2c16d01493d9df33baca46710db8c0f
Original-pokemon <eremkin-02@mail.ru>
2023-09-03 15:32:00 +0300

    feat: api-action

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,19 +1,37 @@
 import { createReducer } from '@reduxjs/toolkit';
 
 import { CityName } from '../const.ts';
-import { offers } from '../mocks/offers.ts';
 import { CityNameType } from '../types/cityName.ts';
-import { Offer } from '../types/offers.ts';
-import { getOffersPerCity, setActiveCity } from './action.ts';
+import { ExtendedOfferType, OfferType } from '../types/offers.ts';
+import { ReviewType } from '../types/reviews.ts';
+import {
+  getExtendedOffer,
+  getFavoriteOffers,
+  getNearOffers,
+  getOffers,
+  getReviews,
+  setActiveCity,
+  setDataLoadingStatus,
+} from './action.ts';
 
-type InitialState = {
+type InitialStateType = {
   activeCity: CityNameType;
-  offers: Offer[];
+  offers: OfferType[];
+  nearOffers: OfferType[];
+  favoriteOffers: OfferType[];
+  reviews: ReviewType[];
+  extendedOffer?: ExtendedOfferType;
+  isDataLoading: boolean;
 }
 
-const initialState: InitialState = {
+const initialState: InitialStateType = {
   activeCity: CityName.Paris,
-  offers: offers,
+  offers: [],
+  nearOffers: [],
+  favoriteOffers: [],
+  reviews: [],
+  extendedOffer: undefined,
+  isDataLoading: false
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -21,8 +39,23 @@ const reducer = createReducer(initialState, (builder) => {
     .addCase(setActiveCity, (state, action) => {
       state.activeCity = action.payload;
     })
-    .addCase(getOffersPerCity, (state) => {
-      state.offers = offers;
+    .addCase(getOffers, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(getNearOffers, (state, action) => {
+      state.nearOffers = action.payload;
+    })
+    .addCase(getFavoriteOffers, (state, action) => {
+      state.favoriteOffers = action.payload;
+    })
+    .addCase(getReviews, (state, action) => {
+      state.reviews = action.payload;
+    })
+    .addCase(getExtendedOffer, (state, action) => {
+      state.extendedOffer = action.payload;
+    })
+    .addCase(setDataLoadingStatus, (state, action) => {
+      state.isDataLoading = action.payload;
     });
 });
 
